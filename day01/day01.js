91c53c1879c62c99145637bc005ee0119ef7057d
zeltbrennt <zeltbrennt@gmail.com>
2023-02-19 11:39:30 +0100

    Show distance

--- day01/day01.js
+++ day01/day01.js
@@ -19,14 +19,24 @@ let part1, part2;
 function setup() {
     createCanvas(1000, 1000);
     //frameRate(1);
-    background(51);
     thisInstruction = instruction[0];
 }
 
 function draw() {
+    background(51);
     translate(920, 250);
     fill(255, 255, 0);
+    stroke(255);
+    line(0, 0, x * scaling, 0);
+    line(x * scaling, 0, x * scaling, y * scaling);
     noStroke();
+    fill(0, 255, 0);
+    circle(0, 0, 4);
+    fill(255, 0, 0);
+    for (let p of visited) circle(p.split(",")[0] * scaling, p.split(",")[1] * scaling, 4);
+    part1 = abs(x) + abs(y);
+    document.querySelector("#part1").innerText = "part 1: " + part1;
+    document.querySelector("#part2").innerText = "part 2: " + part2;
     if (turn && corner === 0) circle(0, 0, 4);
     if (turn) {
         if (thisInstruction.startsWith("R")) direction = direction === 3 ? 0 : direction + 1;
@@ -39,19 +49,14 @@ function draw() {
         corner++;
         thisInstruction = instruction[corner];
         if (corner === instruction.length) {
-            part1 = abs(x) + abs(y);
             fill(0, 255, 0);
             circle(x * scaling, y * scaling, 4);
             noLoop();
-            console.log("part1: " + part1);
-            console.log("part2: " + part2);
         }
     } else {
         x += direction % 2 === 0 ? 0 : direction === 1 ? 1 : -1;
         y += direction % 2 !== 0 ? 0 : direction === 2 ? 1 : -1;
         steps--;
-        fill(255, 0, 0);
-        circle(x * scaling, y * scaling, scaling);
         if (visited.has(x + "," + y) && !part2) {
             part2 = abs(x) + abs(y);
             fill(255, 255, 0);
