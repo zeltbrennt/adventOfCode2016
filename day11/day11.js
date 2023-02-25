397a4f0b4a9664e1a7817d8af71c2a3290b83b07
zeltbrennt <zeltbrennt@gmail.com>
2023-02-25 21:30:59 +0100

    Rewrite working for example

--- day11/day11.js
+++ day11/day11.js
@@ -1,65 +1,73 @@
 /*
-    
-4   0   0   0   0   0   0   0   0
-3   0   0   0   0   0   0   0   0
-2   0   0   0   1   0   1   1   1
-1   1   1   1   0   1   0   0   0
+    thG thM plG plM stG stM prG prM ruG ruM 
+4   0   0   0   0   0   0   0   0   0   0   
+3   0   0   0   0   0   0   1   1   1   1   
+2   0   0   0   1   0   1   0   0   0   0   
+1   1   1   1   0   1   0   0   0   0   0   
 
-swap last   xor 1 << 0 & 1 << 4
-swap second xor 1 << 1 & 1 << 5
-swap third  xor 1 << 2 & 1 << 6
-swap fourth xor 1 << 3 & 1 << 7
 
-swap 1 & 2 << 0
-swap 2 & 3 << 4
-swap 3 & 4 << 8
-
-bitmap full floor    15 << 0 / 4 / 8 / 12
-bitmap forbidden    1010 / 1011 / 0110 / 0111
 */
-const puzzle = 645;
+const puzzle = "5-8-2-0-0";
+//const puzzle = "928-80-31-0-0";
+//const word = 10;
 const word = 4;
-const floorMap = (1 << word) - 1;
-let elevator = 0;
 const options = [];
-for (let x = 1; x < 1 << word + 1; x++) {
+for (let x = 1; x < 1 << word; x++) {
     if ((x).toString(2).replace(/0/g, "").length <= 2) options.push(x)
 }
-const forbidden = [0b1001, 0b1101, 0b0110, 0b0111];
-const target = (floorMap << 12 ) + 0.125;
-
+//const forbidden = [0b1001, 0b1101, 0b0110, 0b0111];
+const forbidden = [];
+const target = "0-0-0-15-3";
+//const target = "0-0-0-0-0-0-0-0-0-1023-3";
 const printFloors = (state) => {
-    console.log(`F4 ${state % 1 == 0.125 ? "E" : "."} ${getFloor(state, 3).toString(2).padStart(word, "0")}`)
-    console.log(`F3 ${state % 1 == 0.25 ? "E" : "."} ${getFloor(state, 2).toString(2).padStart(word, "0")}`)
-    console.log(`F2 ${state % 1 == 0.5 ? "E" : "."} ${getFloor(state, 1).toString(2).padStart(word, "0")}`)
-    console.log(`F1 ${state % 1 == 0 ? "E" : "."} ${getFloor(state, 0).toString(2).padStart(word, "0")}`)
+    const floors = state.split("-").map(Number);
+    const elevator = floors.pop();
+    floors.reverse().forEach((x, i) => console.log(`F${word - i} ${(word - 1 - i) == elevator ? "E" : "."} ${x.toString(2).padStart(word, "0")}`))
     console.log();
 }
 
-// given a set off all options and a floor, return every bitmap of possible movements
-const getSwaps = (state) => {
-    const swaps = [];
-    let elevator = state % 1;
-    switch (elevator) {
-        case 0.5: elevator = 1; break;
-        case 0.25: elevator = 2; break;
-        case 0.125: elevator = 3; break;
+const validFloor = (floor) => {
+    //if (0b1010101010 & floor == 0) return true;
+    if (0b1010 & floor == 0) return true;
+    for (let i = 0; i < word; i+=2) {
+        if ((1 << i & floor) == 0) continue; 
+        if ((1 << (i + 1) & floor) == 0) return false;
     }
-    const floor = getFloor(state, elevator);
-    for (combo of options) {
-        if (combo === (combo & floor)) {
-            if (elevator < 3 && !forbidden.includes(getFloor(state, elevator + 1) ^ combo & floor)) 
-            swaps.push({"nextFloor" : elevator+1, "bitmap": (combo ^ combo << word) << (word * elevator)});
-            if (elevator > 0 && !forbidden.includes(getFloor(state, elevator - 1) ^ combo & floor)) 
-            swaps.push({"nextFloor" :elevator-1, "bitmap": (combo ^ combo << word) << (word * (elevator - 1))});
-        }
-    }
-    return swaps;
+    return true;
 }
 
-// given a state of floors and a floor number, return a subset of bits of this floor
-const getFloor = (state, n) => {
-    return (state & floorMap << n * word) >> n * word;
+
+// given a set off all options and a floor, return every bitmap of possible movements
+const getNextMoves = (state) => {
+    const floors = state.split("-").map(Number);
+    const moves = [];
+    const elevator = floors.pop();
+    const floor = floors[elevator];
+    const fa = floors[elevator + 1];
+    const fb = floors[elevator - 1];
+    for (combo of options) {
+        if (combo === (combo & floor)) {
+            if (elevator < 3 && validFloor(fa ^ combo)) {
+                floors[elevator] = floor ^ combo;
+                floors[elevator + 1] = fa ^ combo;
+                let newState = "";
+                floors.forEach(x => newState += x + "-");
+                moves.push(newState + (elevator + 1));
+                floors[elevator + 1] = fa;
+                floors[elevator] = floor;
+            }
+            if (elevator > 0 && validFloor(fb ^ combo)) {
+                floors[elevator] = floor ^ combo;
+                floors[elevator - 1] = fb ^ combo;
+                let newState = "";
+                floors.forEach(x => newState += x + "-");
+                moves.push(newState + (elevator - 1));
+                floors[elevator - 1] = fb;
+                floors[elevator] = floor;
+            }
+        }
+    }
+    return moves;
 }
 
 const connections = {};
@@ -70,9 +78,9 @@ const search = () => {
     connections[state] = state;
     while (queue.length != 0) {
         state = queue.shift();
+       // printFloors(state);
         if (state === target) break;
-        for (swap of getSwaps(state, elevator)) {
-            const newState = (state ^ swap.bitmap) + (swap.nextFloor == 0 ? 0 : 1 / (1 << swap.nextFloor));
+        for (newState of getNextMoves(state)) {
             if (!visited.has(newState)) {
                 queue.push(newState);
                 visited.add(newState);
@@ -90,7 +98,9 @@ while (puzzle != current) {
     path.push(connections[current]);
     current = connections[current];
 }
+printFloors(target);
 for (p of path) {
     printFloors(p);
 }
 console.log(path.length);
+
