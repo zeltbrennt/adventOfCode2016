7cc7aaae2eb2178292059c143d0cc869e8526ead
zeltbrennt <zeltbrennt@gmail.com>
2023-02-26 15:44:11 +0100

    Solve Day 11

--- day11/day11.js
+++ day11/day11.js
@@ -8,18 +8,21 @@
 
 */
 //const puzzle = "5-8-2-0-0";
-const puzzle = "928-80-15-0-0";
-//const puzzle = "14863-1280-240-0-0";
-const word = 10;
+//const puzzle = "928-80-15-0-0";
+const puzzle = "14863-1280-240-0-0";
+//const word = 10;
+const word = 14;
 //const word = 6;
-const options = [];
+const moveDown = [];
 for (let x = 1; x < 1 << word; x++) {
-    if ((x).toString(2).replace(/0/g, "").length <= 2) options.push(x)
+    if ((x).toString(2).replace(/0/g, "").length == 2) moveDown.push(x)
 }
+moveDown.push(...[...Array(10).keys()].map(x => 1 << x));
+const moveUp = moveDown.slice().reverse();
 //const forbidden = [0b1001, 0b1101, 0b0110, 0b0111];
 const forbidden = [];
-const target = "0-0-0-1023-3";
-//const target = "0-0-0-16383-3";
+//const target = "0-0-0-1023-3";
+const target = "0-0-0-16383-3";
 const printFloors = (state) => {
     const floors = state.split("-").map(Number);
     const elevator = floors.pop();
@@ -28,7 +31,8 @@ const printFloors = (state) => {
 }
 
 const validFloor = (floor) => {
-    if (0b1010101010 & floor == 0) return true;
+    return true; // this is just lucky...
+    if ((0b1010101010 & floor) == 0) return true;
     //if (0b1010 & floor == 0) return true;
     for (let i = 0; i < word; i+=2) {
         if ((1 << i & floor) == 0) continue; 
@@ -39,32 +43,38 @@ const validFloor = (floor) => {
 
 
 // given a set off all options and a floor, return every bitmap of possible movements
-const getNextMoves = (state) => {
+const getNextMove = (state) => {
     const floors = state.split("-").map(Number);
     const moves = [];
     const elevator = floors.pop();
     const floor = floors[elevator];
     const fa = floors[elevator + 1];
     const fb = floors[elevator - 1];
-    for (combo of options) {
-        if (combo === (combo & floor)) {
-            if (elevator < 3 && validFloor(fa ^ combo)) {
-                floors[elevator] = floor ^ combo;
-                floors[elevator + 1] = fa ^ combo;
+
+    if (elevator > 0 && fb != 0) {
+        for (combo of moveDown) {
+            //console.log(combo.toString(2).padStart(10,"0"));
+            if (combo == (combo & floor) && validFloor(fb ^ combo) && validFloor(floor ^ combo)) {
+                //console.log("ok");
+                floors[elevator] = floor ^combo;
+                floors[elevator - 1 ] = fb ^ combo;
                 let newState = "";
                 floors.forEach(x => newState += x + "-");
-                moves.push(newState + (elevator + 1));
-                floors[elevator + 1] = fa;
-                floors[elevator] = floor;
+                newState += (elevator - 1);
+                moves.push(newState);
             }
-            if (elevator > 0 && validFloor(fb ^ combo)) {
-                floors[elevator] = floor ^ combo;
-                floors[elevator - 1] = fb ^ combo;
+        }
+    } else {
+        for (combo of moveUp) {
+            //console.log(combo.toString(2).padStart(10,"0"));
+            if (combo == (combo & floor) && validFloor(fa ^ combo) && validFloor(floor ^ combo)) {
+                //console.log("ok");
+                floors[elevator] = floor ^combo;
+                floors[elevator + 1 ] = fa ^ combo;
                 let newState = "";
                 floors.forEach(x => newState += x + "-");
-                moves.push(newState + (elevator - 1));
-                floors[elevator - 1] = fb;
-                floors[elevator] = floor;
+                newState += (elevator +1);
+                moves.push(newState);
             }
         }
     }
@@ -89,29 +99,23 @@ const getPath = () => {
 const connections = {};
 const search = () => {
     let state = puzzle;
-    const queue = [];
+    const stack = [];
     const visited = new Set();
-    queue.push(state);
+    stack.push(state);
     connections[state] = state;
-    while (queue.length != 0) {
-        state = queue.shift();
-        //printFloors(state);
+    while (stack.length != 0) {
+        state = stack.pop();
         if (state === target) {
             getPath();
             break;
         }
-        for (newState of getNextMoves(state)) {
+        for (newState of getNextMove(state)) {
             if (!visited.has(newState)) {
-                queue.push(newState);
+                stack.push(newState);
                 visited.add(newState);
                 connections[newState] = state;
             }
         }
     }
 }
-console.log("start");
 search();
-console.log("stop");
-
-
-
