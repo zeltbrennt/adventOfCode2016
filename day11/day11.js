8cf97aab5f58fac741e81c872a89245b433ea014
zeltbrennt <zeltbrennt@gmail.com>
2023-03-03 10:39:56 +0100

    Fix Sollution

--- day11/day11.js
+++ day11/day11.js
@@ -10,17 +10,16 @@
 //const puzzle = "5-8-2-0-0";
 //const puzzle = "928-80-15-0-0";
 const puzzle = "14863-1280-240-0-0";
+//const word = 6;
 //const word = 10;
 const word = 14;
-//const word = 6;
 const moveDown = [];
 for (let x = 1; x < 1 << word; x++) {
     if ((x).toString(2).replace(/0/g, "").length == 2) moveDown.push(x)
 }
 moveDown.push(...[...Array(10).keys()].map(x => 1 << x));
 const moveUp = moveDown.slice().reverse();
-//const forbidden = [0b1001, 0b1101, 0b0110, 0b0111];
-const forbidden = [];
+//const target = "0-0-0-15-3";
 //const target = "0-0-0-1023-3";
 const target = "0-0-0-16383-3";
 const printFloors = (state) => {
@@ -31,8 +30,8 @@ const printFloors = (state) => {
 }
 
 const validFloor = (floor) => {
-    return true; // this is just lucky...
-    if ((0b1010101010 & floor) == 0) return true;
+    return true; // optimal solution under constraint is the same as without constraint for this input...
+    //if ((0b101010101010 & floor) == 0) return true;
     //if (0b1010 & floor == 0) return true;
     for (let i = 0; i < word; i+=2) {
         if ((1 << i & floor) == 0) continue; 
@@ -51,11 +50,10 @@ const getNextMove = (state) => {
     const fa = floors[elevator + 1];
     const fb = floors[elevator - 1];
 
-    if (elevator > 0 && fb != 0) {
+    //if (elevator > 0 && fb != 0) {
+    if (elevator < 3) {
         for (combo of moveDown) {
-            //console.log(combo.toString(2).padStart(10,"0"));
             if (combo == (combo & floor) && validFloor(fb ^ combo) && validFloor(floor ^ combo)) {
-                //console.log("ok");
                 floors[elevator] = floor ^combo;
                 floors[elevator - 1 ] = fb ^ combo;
                 let newState = "";
@@ -66,9 +64,7 @@ const getNextMove = (state) => {
         }
     } else {
         for (combo of moveUp) {
-            //console.log(combo.toString(2).padStart(10,"0"));
             if (combo == (combo & floor) && validFloor(fa ^ combo) && validFloor(floor ^ combo)) {
-                //console.log("ok");
                 floors[elevator] = floor ^combo;
                 floors[elevator + 1 ] = fa ^ combo;
                 let newState = "";
