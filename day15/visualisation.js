076e74ca16f92b075569b8df77832c1c6b54a463
zeltbrennt <zeltbrennt@gmail.com>
2023-03-03 11:09:33 +0100

    Add Visualization

--- day15/visualisation.js
+++ day15/visualisation.js
@@ -11,21 +11,21 @@ let frame = 0;
 function setup() {
     createCanvas(400, 400);
     frameRate(1);
-
 }
 
 function draw() {
     ellipseMode(CENTER)
     background(220);
     translate(80, 80);
-    for (let i = O; i < disks.length; i++) {
-        fill(230, 230, O, 100);
+    for (let i = 0; i < disks.length; i++) {
+        fill(230, 230, 0, 100);
         disks[i].pos = (disks[i].pos + 1) % disks[i].size;
-        rot[i] = disks.pos / disks.size;
+        rot[i] = disks[i].pos / disks[i].size;
+
         push();
         translate(i * 50, i * 50);
-        rotate(rot);
-        arc(100, 100, 0, PI - TWO_PI / disks[i].size, PIE);
+        rotate(TWO_PI * rot[i]);
+        arc(0, 0, 100, 100, 0, TWO_PI - TWO_PI / disks[i].size, PIE);
         stroke(255, 0, 0);
         pop();
     }
