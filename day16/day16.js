86321b0542eb0fd7e02676eca9dcdf0b07035cb5
zeltbrennt <zeltbrennt@gmail.com>
2023-03-03 10:41:09 +0100

    Fix Sollution

--- day16/day16.js
+++ day16/day16.js
@@ -1,19 +1,19 @@
 const dragonCurve = (str, minChar) => {
     do {
-        str = str + "0" + str.split("").map(x => x == 1 ? 0 : 1).reverse().join("");
+        str = str + "0" + str.split("").map(x => x === "1" ? "0" : "1").reverse().join("");
     } while (str.length < minChar);
-    return str.substr(0, minChar);
+    return str.slice(0, minChar);
 }
 
 const checkSum = (str) => {
     do {
         let check = [];
         for (let i = 0; i < str.length; i += 2) {
-            if (str[i] == str[i+1]) check.push(1);
+            if (str[i] === str[i+1]) check.push(1);
             else check.push(0);
         }
         str = check.join("");
-    } while (str.length % 2 == 0);
+    } while (str.length % 2 === 0);
     return str;
 }
 
